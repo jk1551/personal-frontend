9736401b61a208c1044ee1e43ee3fe443b6eca8f
Joe King <joe.k659@gmail.com>
2022-04-18 11:39:45 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Test for each page tab coponent that it has a title and a list of items
+
+// Test API calls
+
+// Test Any other functions / logic
